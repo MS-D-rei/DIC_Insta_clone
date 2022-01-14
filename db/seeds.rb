58c26a16a91424817fa6117b4e88216088d67c30
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-01-14 17:48:16 +0900

    add post sample data with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: 'Test Name',
+  email: 'test@mail.com',
+  password: 'foobar01',
+  password_confirmation: 'foobar01'
+)
+
+50.times do |n|
+  name = Faker::Name.name
+  email = "test_#{n + 1}@mail.com"
+  password = 'password'
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password
+  )
+end
+
+users = User.order(:created_at).take(5)
+30.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.posts.create!(content: content) }
+end
