14d61473327179a63e1880ebd59c6db92869138d
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-01-15 12:06:38 +0900

    add post edit and update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: %i[new create edit update destroy]
+  before_action :correct_user, only: %i[edit update destroy]
 
   def new
     @post = current_user.posts.new
@@ -15,9 +16,33 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = 'Post updated!'
+      redirect_to root_url
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = 'Post deleted'
+    redirect_to root_url
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to root_url if @post.nil?
   end
 end
